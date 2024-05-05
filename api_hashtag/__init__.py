2f6b0a047d18e1a538d3453b795741003bb0702a
barbara-sz <120216936+barbara-sz@users.noreply.github.com>
2024-05-05 19:22:31 -0300

    Update __init__.py

--- api_hashtag/__init__.py
+++ api_hashtag/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
 from dotenv import load_dotenv
+import sqlalchemy
 
 load_dotenv()
 
@@ -20,6 +21,15 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message = "Faça login para continuar"
-login_manager.login_message_category = "danger"
+login_manager.login_message_category = "alert-info"
+
+from api_hashtag import models
+
+engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("log"):
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
 
 from api_hashtag import routes
